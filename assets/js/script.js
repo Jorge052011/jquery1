3f1bd5e012279ddb9655e78f29a83ceba33366c9
Jorge052011 <jimunozacuna+1@gmail.com>
2025-04-24 03:03:31 +0000

    algo salio mal en el script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,18 @@
 console.log('jQuery versión:', $.fn.jquery);
 console.log('Bootstrap Modal existe?', typeof bootstrap.Modal);
 
+$("p").click(function(){
 
+    $(this).hide(4000)
+    $(this).fadeIn(4000)
+})
 
+$("#boton").click(function(){
 
-
-//variables en js
-let edad = 30
-let edad2 = 35
-var nombre = "juan dhfd udfr rurfrrhuehueeguue"
-let numerogrande = 38507657650873650073486
-let nombre2 = "dddhfhfjfddjjdjdjdjdjdjd"
-const rut = 2344566777
-
-
-console.log(rut)
-
-
-//tipos de datos
-let texto = "este es un texto";
-let numero = 2
-let esestudiante = false
-let nulo = null
-let indefinido;
-let numerogrande2 = 445328282929383746464;
-
-
-
-
-
-//operadores
-let a = 10 + 5
-
-let numero1 = 10
-let numero2 = 5
-let resultado = numero1 + numero2
-
-
-//operadores
-var suma = 10 + 5
-
-var resta = 10 - 5
-
-var multiplicacion = 10 * 5
-
-var division = 10 / 5
-
-var modulo = 10 % 5
-
-
-
-//estructuras de control
-
-//if else
-
-let nota = 30
-if (nota>= 90) {
-    console.log("muy buena nota")
-} else if (nota>=50) {
-    console.log("nota normal")
-} else {
-    console.log("reprobado")
-}
-
-//bucle
-
-for (let index = 0; index < 10; index++) {
-    console.log("index = ",index)
-    //alert("dando vuelta")
-}
-        
-
-//manipulacion del DOM 
-
-const div_test = document.getElementById("div_test")
-div_test.innerHTML = "<p> hola desde js</p>"
-
-const div =  document.getElementById("div")
-const btn = document.getElementById("btn")
-
-btn.addEventListener('click' , () => {
-    div.style.backgroundColor = 'red'   
+    $("#div").css({
+        'width: 120px',
+        'background-color': ' #fff'
+    })
 })
 
 
